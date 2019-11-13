02ed27108c54e846d9362d93d7e97fca11818ecf
Unicorn Rentals <elasticvolcano@gmail.com>
2019-11-13 13:27:30 +0000

    Refactored unicorn antagonization code

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ Probably better way to do this, but for now this will do.
 """
 
 __author__ = 'Inigo Montoya (inigo.montoya@unicornrentals.click)'
-__vcs_id__ = '12aff8252fe2fe88bc2509aa21d13fa1'
-__version__ = '3f1905aab41b07429cb603d8965d3e0e'
+__vcs_id__ = 'a167188985a12924cc2cc3c3d879eea5'
+__version__ = '3bbe28d7d3b28e6f97b0b080ec15c4eb'
 
 from flask import Flask, request
 from flask_restful import Resource, Api
